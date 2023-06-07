17f4a606688582a5d6b49ebc35562083e2eb316c
Wanderlee Max <wanderleedev@gmail.com>
2023-06-06 21:59:46 -0500

    Estructura base HTMl lista, moulos JS creados y agregados

---
+++ components/Description/Description.js
@@ -0,0 +1,13 @@
+const Description =`
+  <div class="container">
+    <div class="container-content">
+      <img loading="lazy" class="container-icon" src='./assets/img/6dots.svg' alt="imagen de postre - receta">
+      <p class="container-description">
+        Look no further for a creamy and ultra smooth classic cheesecake recipe! Paired with a buttery graham cracker crust, no one can deny its simple decadence. For the best results, bake in a water bath.
+      </p>
+    </div>
+    <img loading="lazy" class="container-img" src='./assets/img/photo1.png' alt="imagen de postre - receta">
+  </div>
+`.trim();
+
+export default Description
